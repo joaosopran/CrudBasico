c78ac03e5b2f5141f7b026a57ef71f97e969238a
joaosopran <159202685+joaosopran@users.noreply.github.com>
2025-06-03 09:34:56 -0300

    Projeto Criado

---
+++ demo/src/test/java/io/github/joaosopran/demo/ClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.joaosopran.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
