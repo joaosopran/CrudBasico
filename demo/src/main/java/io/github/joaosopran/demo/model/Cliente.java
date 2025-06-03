8808ddf39f061b394571f5375b4c33c02c2e06f1
joaosopran <159202685+joaosopran@users.noreply.github.com>
2025-06-03 09:42:51 -0300

    Cliente criado, e configuração de banco de dados H2

---
+++ demo/src/main/java/io/github/joaosopran/demo/model/Cliente.java
@@ -0,0 +1,23 @@
+package io.github.joaosopran.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String CPF;
+    private String CEP;
+}
